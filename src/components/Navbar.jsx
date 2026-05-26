3f4769cefacb181bec9d2fe0652892b4d07bc626
Didarul Alam <didarulalamrafi1312@gmil.com>
2026-05-26 19:52:21 +0600

    signUp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="border-3 p-4 bg-gray mx-50">
+    <div className="flex justify-around border-3 p-4 bg-gray mx-50">
       <Link className="mr-10" href="/">
         Home
       </Link>
       <Link href="/models">Model</Link>
+      <Link href={"/signup"}>Sign Up</Link>
+      <Link href={"/signin"}>Sign In</Link>
     </div>
   );
 };
