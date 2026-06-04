e18940c3dcd63943a4f92bd391e723f05de1a079
Didarul Alam <didarulalamrafi1312@gmil.com>
2026-06-04 23:57:46 +0600

    sign out

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,76 +1,4 @@
-// "use client";
-// import { Check } from "@gravity-ui/icons";
-// import {
-//   Button,
-//   Description,
-//   FieldError,
-//   Form,
-//   Input,
-//   Label,
-//   TextField,
-// } from "@heroui/react";
-
-// const SignInPage = () => {
-//   return (
-//     <Form className="flex w-96 flex-col gap-4 rounded-2xl mx-auto mt-10 p-8 shadow-2xl">
-//       {/* Email */}
-//       <TextField
-//         isRequired
-//         name="email"
-//         type="email"
-//         validate={(value) => {
-//           if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-//             return "Please enter a valid email address";
-//           }
-//           return null;
-//         }}
-//       >
-//         <Label>Email</Label>
-//         <Input placeholder="john@example.com" />
-//         <FieldError />
-//       </TextField>
-//       <TextField
-//         isRequired
-//         minLength={8}
-//         name="password"
-//         type="password"
-//         validate={(value) => {
-//           if (value.length < 8) {
-//             return "Password must be at least 8 characters";
-//           }
-//           if (!/[A-Z]/.test(value)) {
-//             return "Password must contain at least one uppercase letter";
-//           }
-//           if (!/[0-9]/.test(value)) {
-//             return "Password must contain at least one number";
-//           }
-//           return null;
-//         }}
-//       >
-//         <Label>Password</Label>
-//         <Input placeholder="Enter your password" />
-//         <Description>
-//           Must be at least 8 characters with 1 uppercase and 1 number
-//         </Description>
-//         <FieldError />
-//       </TextField>
-//       <div className="flex gap-2">
-//         <Button type="submit">
-//           <Check />
-//           Submit
-//         </Button>
-//         <Button type="reset" variant="secondary">
-//           Reset
-//         </Button>
-//       </div>
-//     </Form>
-//   );
-// };
-
-// export default SignInPage;
-
 "use client";
-
 import { authClient } from "@/lib/auth-client";
 import { Check } from "@gravity-ui/icons";
 import {
@@ -82,21 +10,22 @@ import {
   Label,
   TextField,
 } from "@heroui/react";
+import { Fallback } from "next/dist/client/components/segment-cache/cache-map";
 // import { authClient } from "@/lib/auth-client";
 
-const Signup = () => {
+const SignIn = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
-    const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(name, email, password);
+    console.log(email, password);
 
-    const { data, error } = await authClient.signUp.email({
-      name,
+    const { data, error } = await authClient.signIn.email({
       email,
       password,
+      callbackURL: "/",
     });
+    console.log(data, error);
   };
 
   return (
@@ -158,4 +87,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default SignIn;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const { data: session } = authClient.useSession();
+  console.log(session);
+  const user = session?.user;
+
+  const handleSignOut = () => {
+    authClient.signOut();
+    console.log("sign out");
+  };
   return (
     <div className="flex justify-around border-3 p-4 bg-gray mx-50">
       <Link className="mr-10" href="/">
@@ -9,8 +20,11 @@ const Navbar = () => {
       </Link>
       <Link href="/models">Model</Link>
       <Link href={"/signup"}>Sign Up</Link>
-      <Link href="/signup">Sign Up 2</Link>
       <Link href={"/signin"}>Sign In</Link>
+      <div>{user?.name}</div>
+      <Button onClick={handleSignOut} variant="primary">
+        Sign Out
+      </Button>
     </div>
   );
 };
